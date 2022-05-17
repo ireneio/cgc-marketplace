483b982c8d4e0b951b22b598b57ea4cfd07ca565
irene <keiko15678@gmail.com>
2022-05-17 17:28:05 +0800

    add select group

--- src/components/Shared/SelectGroup.tsx
+++ src/components/Shared/SelectGroup.tsx
@@ -28,15 +28,19 @@ const SelectGroup = ({ items, currentValue, onItemClick }: Props) => {
                 borderBottomLeftRadius: index === 0 ? '4px' : 0,
                 borderTopRightRadius: index === items.length - 1 ? '4px' : 0,
                 borderBottomRightRadius: index === items.length - 1 ? '4px' : 0,
-                backgroundColor:
-                  currentValue === item.value ? '#FC1F8E' : '#13002B',
+                backgroundColor: item.disabled
+                  ? '#181818'
+                  : currentValue === item.value
+                  ? '#FC1F8E'
+                  : '#13002B',
                 cursor: item.disabled
                   ? 'not-allowed'
                   : currentValue === item.value
                   ? 'default'
                   : 'pointer',
               }}
-              className="border-solid border-[1px] border-[#FC1F8E] px-[16px] py-[8px]"
+              disabled={item.disabled}
+              className="border-solid border-[1px] border-[#FC1F8E] px-[16px] py-[8px] disabled:bg-[#181818] disabled:text-[#AAA]"
             >
               {item.text}
             </button>
