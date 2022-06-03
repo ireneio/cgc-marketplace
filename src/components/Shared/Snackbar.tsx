a4728ca258ee8644a286053c74855e0034540d37
irene <keiko15678@gmail.com>
2022-06-03 13:45:43 +0800

    fix xnackbar

--- src/components/Shared/Snackbar.tsx
+++ src/components/Shared/Snackbar.tsx
@@ -25,33 +25,40 @@ const Snackbar = ({
         >
           <div className="bg-transparent text-center py-4 lg:px-4">
             <div
-              className="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex"
-              role="alert"
+              style={{
+                background: 'linear-gradient(180deg, #F41786 0%, #A713ED 100%)',
+              }}
+              className="px-[1px] py-[1px] flex items-center justify-center lg:rounded-full"
             >
-              <span className="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">
-                {title}
-              </span>
-              <span className="font-semibold mr-2 text-left flex-auto">
-                {text}
-              </span>
               <div
-                className="cursor-pointer mt-[3px]"
-                onClick={() => dispatch({ type: 'CLOSE_SNACKBAR' })}
+                className="p-2 bg-[#13002B] items-center text-[#FFFFFF] leading-none lg:rounded-full flex lg:inline-flex shadow-lg"
+                role="alert"
               >
-                <img
-                  src="/img/icon_purple_rounded_close.png"
-                  alt="close"
-                  width={16}
-                  height={16}
-                />
-              </div>
-              {/* <svg
+                <span className="flex rounded-full bg-[#290030] uppercase px-2 py-1 text-xs font-bold mr-3">
+                  {title}
+                </span>
+                <span className="font-semibold mr-2 text-left flex-auto">
+                  {text}
+                </span>
+                <div
+                  className="cursor-pointer mt-[3px]"
+                  onClick={() => dispatch({ type: 'CLOSE_SNACKBAR' })}
+                >
+                  <img
+                    src="/img/icon_purple_rounded_close.png"
+                    alt="close"
+                    width={16}
+                    height={16}
+                  />
+                </div>
+                {/* <svg
                     className="fill-current opacity-75 h-4 w-4"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                   >
                     <path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" />
                   </svg> */}
+              </div>
             </div>
           </div>
         </motion.div>
