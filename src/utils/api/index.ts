5c59b22d327d0bf7d6776129be1a3a0f4551fc0b
irene <keiko15678@gmail.com>
2022-06-06 22:11:04 +0800

    example api hook up

--- src/utils/api/index.ts
+++ src/utils/api/index.ts
@@ -1,4 +1,9 @@
-import { fetcher } from '../swr';
+import { API_URL, fetcher } from '../swr';
+
+const CLIENT_ID = API_URL.includes('dev')
+  ? '773124bb-57cf-4eae-a7be-980c76ccd338'
+  : '';
+const CLIENT_SECRET = API_URL.includes('dev') ? '1qaz2wsx' : '';
 
 const api = {
   healthCheck: async () => {
@@ -11,8 +16,8 @@ const api = {
       url: '/api/oauth2/token',
       method: 'post',
       data: {
-        client_id: '773124bb-57cf-4eae-a7be-980c76ccd338',
-        client_secret: '1qaz2wsx',
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
         grant_type: 'authorization_code',
       },
     });
