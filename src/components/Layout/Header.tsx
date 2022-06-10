c6ee0b4c055503b24827c7c25605992de5c8cfa4
irene <keiko15678@gmail.com>
2022-06-10 17:49:04 +0800

    fix input

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -34,7 +34,7 @@ const Header = () => {
             type="text"
             value={search}
             onChange={(e) => handleSearchInput(e.target.value)}
-            className="ml-[6px] bg-[#0C001C] w-full font-circularstdbook appearance-none border border-[#290030] rounded-md py-2 px-4 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-[#FC1F8E] focus:border-white focus:placeholder-gray-400"
+            className="ml-[6px] text-[#FFF] bg-[#0C001C] w-full font-circularstdbook appearance-none border border-[#290030] rounded-md py-2 px-4 text-base placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-[#FC1F8E] focus:border-white focus:placeholder-gray-400"
             placeholder="Search Games and Collections"
           />
           <div className="absolute right-[12px] top-[12px]">
