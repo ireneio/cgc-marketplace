14fd4af38d391410aa827fd272b39fec14aafc52
Jeansy Molanza <jeansymolanza@hotmail.co.uk>
2022-06-11 08:37:14 +0100

    fix typo in collection reducer filename

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import user from './user';
 import layout from './layout';
 import cart from './cart';
-import collection from './colletion';
+import collection from './collection';
 
 const rootReducer = combineReducers({
   user,
