9d7c7a497566fc4d31b4b81438147abadf6df880
irene <keiko15678@gmail.com>
2022-06-12 10:31:07 +0800

    update ui

--- src/components/Collection/CollectionCarousel.tsx
+++ src/components/Collection/CollectionCarousel.tsx
@@ -66,15 +66,15 @@ const CollectionCarousel = ({ carouselItems }: { carouselItems: Item[] }) => {
                     <source src={imageUrl} type="video/mp4" />
                   </video>
                 </div>
-                <div className="z-[3] absolute bottom-[220px] lg:bottom-[40%] left-[16px] md:left-[50px] font-bold text-[32px]">
+                <div className="max-w-[80vw] z-[3] absolute top-[50px] md:top:-[80px] lg:top-[200px] left-[16px] md:left-[50px] font-bold text-[32px]">
                   <img
                     src={logo}
                     className="h-[60px] 2xl:h-[80px] max-w-[80vw] bg-transparent aspect-w-1 aspect-h-1 rounded-[5px] overflow-hidden transform transition duration-500 aspect-none hover:cursor-pointer"
                     alt={name}
                   />
                 </div>
-                <div className="block text-left z-[3] absolute bottom-[100px] md:bottom-[160px] lg:bottom-[30%] left-[16px] md:left-[50px] text-[#FFFFFF] text-xl">
-                  {description?.slice(0, 201)}
+                <div className="max-w-[70vw] block text-left z-[3] absolute top-[120px] md:top-[150px] lg:top-[290px] left-[16px] md:left-[50px] text-[#FFFFFF] text-xl">
+                  {description?.slice(0, 150)}
                 </div>
                 <div className="z-[3] absolute bottom-[12px] lg:bottom-[17.5%] left-[16px] md:left-[50px] text-[#FFFFFF] flex">
                   <Button onClick={() => handlePlay(name)}>Play</Button>
