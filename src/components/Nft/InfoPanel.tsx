250b1860c25c49183f5819a0355bf0f8abfe1739
irene <keiko15678@gmail.com>
2022-06-15 04:42:01 +0800

    adjust nft page

--- src/components/Nft/InfoPanel.tsx
+++ src/components/Nft/InfoPanel.tsx
@@ -10,26 +10,34 @@ const InfoPanel = ({ info }: { info: NftInfo }) => {
       <div>
         <div className="flex justify-between">
           <div className="text-[14px] text-[#FFFFFF]">Mint Address</div>
-          <PrimaryGradientText className="text-[14px]">
-            <ClipboardText copyValue={info.mintAddress}>
-              {getTrimmedAddressEllipsisMiddle(info.mintAddress, {
-                length: 15,
-              })}
-            </ClipboardText>
-          </PrimaryGradientText>
+          {info?.mintAddress ? (
+            <PrimaryGradientText className="text-[14px]">
+              <ClipboardText copyValue={info.mintAddress}>
+                {getTrimmedAddressEllipsisMiddle(info.mintAddress, {
+                  length: 15,
+                })}
+              </ClipboardText>
+            </PrimaryGradientText>
+          ) : (
+            '-'
+          )}
         </div>
       </div>
       <div className="mt-[14px] flex justify-between">
         <div className="text-[14px] text-[#FFFFFF]">Owner</div>
-        <PrimaryGradientText className="text-[14px]">
-          <ClipboardText copyValue={info.owner}>
-            {getTrimmedAddressEllipsisMiddle(info.owner, { length: 15 })}
-          </ClipboardText>
-        </PrimaryGradientText>
+        {info.owner ? (
+          <PrimaryGradientText className="text-[14px]">
+            <ClipboardText copyValue={info.owner}>
+              {getTrimmedAddressEllipsisMiddle(info.owner, { length: 15 })}
+            </ClipboardText>
+          </PrimaryGradientText>
+        ) : (
+          '-'
+        )}
       </div>
       <div className="mt-[14px] flex justify-between">
         <div className="text-[14px] text-[#FFFFFF]">Royalties</div>
-        <div className="text-[14px] text-[#FFFFFF] font-semibold">
+        <div className="text-[14px] text-[#FFFFFF]">
           {isNaN(info.royaltiesPercentage)
             ? '-'
             : info.royaltiesPercentage + '%'}
