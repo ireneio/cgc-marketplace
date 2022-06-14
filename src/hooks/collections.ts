5d81d6787518df20aa44fc63f0299607e2053166
irene <keiko15678@gmail.com>
2022-06-15 05:35:56 +0800

    adjust api error catching

--- src/hooks/collections.ts
+++ src/hooks/collections.ts
@@ -11,35 +11,39 @@ export const useGetCollections = () => {
   const [loading, setLoading] = useState(false);
 
   const getCollections = async () => {
-    setLoading(true);
-    const response = await api.getCollectionList(oAuthCtx.access_token);
-    const map =
-      response && response.length
-        ? response.map((item: any) => {
-            return {
-              ...item,
-              splashSrc: item.metadata.splashSrcUrl,
-              logoSrc: item.metadata.logoSrcUrl,
-              videoSrc: item.metadata.videoSrcUrl,
-              name: item.metadata.name,
-              slug: item.metadata.name.toLowerCase().split(' ').join('_'),
-              tags: item.tags.length
-                ? item.tags.map((item: any) => item.tag)
-                : [],
-              genre: [item.metadata.genre, ...item.tags.slice(0, 2)],
-              services: item.services,
-              description: item.metadata.description,
-              totalSupply: item?.nftCollectionStats?.totalSupply || null,
-              marketCap: item?.nftCollectionStats?.usdMarketCap || null,
-              network: 'SOL',
-            };
-          })
-        : [];
-    dispatch({ type: 'SET_COLLECTIONS', payload: map });
-    setItems(map);
+    try {
+      const response = await api.getCollectionList(oAuthCtx.access_token);
+      const map =
+        response && response.length
+          ? response.map((item: any) => {
+              return {
+                ...item,
+                splashSrc: item.metadata.splashSrcUrl,
+                logoSrc: item.metadata.logoSrcUrl,
+                videoSrc: item.metadata.videoSrcUrl,
+                name: item.metadata.name,
+                slug: item.metadata.name.toLowerCase().split(' ').join('_'),
+                tags: item.tags.length
+                  ? item.tags.map((item: any) => item.tag)
+                  : [],
+                genre: [item.metadata.genre, ...item.tags.slice(0, 2)],
+                services: item.services,
+                description: item.metadata.description,
+                totalSupply: item?.nftCollectionStats?.totalSupply || null,
+                marketCap: item?.nftCollectionStats?.usdMarketCap || null,
+                network: 'SOL',
+              };
+            })
+          : [];
+      dispatch({ type: 'SET_COLLECTIONS', payload: map });
+      setItems(map);
+    } catch {
+      return;
+    }
   };
 
   useEffect(() => {
+    setLoading(true);
     getCollections()
       .then(() => setLoading(false))
       .catch(() => setLoading(false));
@@ -67,32 +71,38 @@ export const useGetCollectionsBySlug = () => {
   }, [router.query.id]);
 
   const getCollectionData = async () => {
-    const response = await api.getCollectionBySlug(
-      oAuthCtx.access_token,
-      slug.split('_').join('_'),
-    );
-    if (response) {
-      setItems(response);
-      dispatch({
-        type: 'SET_CURRENT_COLLECTION',
-        payload: {
-          ...response,
-          metadata: {
-            ...response.metadata,
-            slug: response.metadata.name.toLowerCase().split(' ').join('_'),
-            id: response.id,
+    try {
+      const response = await api.getCollectionBySlug(
+        oAuthCtx.access_token,
+        slug.split('_').join('_'),
+      );
+      if (response) {
+        setItems(response);
+        dispatch({
+          type: 'SET_CURRENT_COLLECTION',
+          payload: {
+            ...response,
+            metadata: {
+              ...response.metadata,
+              slug: response.metadata.name.toLowerCase().split(' ').join('_'),
+              id: response.id,
+            },
           },
-        },
-      });
+        });
+      }
+    } catch {
+      return;
     }
   };
 
   useEffect(() => {
     if (slug) {
       setLoading(true);
-      getCollectionData().then(() => {
-        setLoading(false);
-      });
+      getCollectionData()
+        .then(() => {
+          setLoading(false);
+        })
+        .catch(() => setLoading(false));
     }
   }, [slug]);
 
@@ -111,41 +121,48 @@ export const useGetNftByCollectionId = () => {
   const [items, setItems] = useState<any[]>([]);
 
   const getData = (token: string) => async (collection_id: string) => {
-    setLoading(true);
-    const response = await api.getNftListByCollectionId(token, collection_id);
-    if (response?.data) {
-      dispatch({
-        type: 'SET_CURRENT_COLLECTION_TOKEN_DATA',
-        payload: response?.data,
-      });
-    }
-    const map =
-      response && response.length
-        ? response.map((item: any) => {
-            const manifest = item?.splNftInfo?.data?.manifest;
-            return {
-              image: manifest?.image,
-              brand: manifest?.collection?.name,
-              name: manifest?.name,
-              description: manifest?.description,
-              price:
-                item?.price ||
-                (item?.external_marketplace_listing &&
-                  item?.external_marketplace_listing.length &&
-                  item?.external_marketplace_listing[0].solPrice),
-              tokenAddress: item?.tokenAddress,
-              is_listed: item?.external_marketplace_listing?.length,
-              external_marketplace_listing: item?.external_marketplace_listing,
-              external_marketplace_listing_logo: item
-                ?.external_marketplace_listing.length
-                ? item?.external_marketplace_listing[0]?.marketplace?.logoSrcUrl
-                : '',
-            };
-          })
-        : [];
+    try {
+      setLoading(true);
+      const response = await api.getNftListByCollectionId(token, collection_id);
+      if (response?.data) {
+        dispatch({
+          type: 'SET_CURRENT_COLLECTION_TOKEN_DATA',
+          payload: response?.data,
+        });
+      }
+      const map =
+        response && response.length
+          ? response.map((item: any) => {
+              const manifest = item?.splNftInfo?.data?.manifest;
+              return {
+                image: manifest?.image,
+                brand: manifest?.collection?.name,
+                name: manifest?.name,
+                description: manifest?.description,
+                price:
+                  item?.price ||
+                  (item?.external_marketplace_listing &&
+                    item?.external_marketplace_listing.length &&
+                    item?.external_marketplace_listing[0].solPrice),
+                tokenAddress: item?.tokenAddress,
+                is_listed: item?.external_marketplace_listing?.length,
+                external_marketplace_listing:
+                  item?.external_marketplace_listing,
+                external_marketplace_listing_logo: item
+                  ?.external_marketplace_listing.length
+                  ? item?.external_marketplace_listing[0]?.marketplace
+                      ?.logoSrcUrl
+                  : '',
+              };
+            })
+          : [];
 
-    setItems(map);
-    setLoading(false);
+      setItems(map);
+      setLoading(false);
+    } catch {
+      setLoading(false);
+      return;
+    }
   };
 
   useEffect(() => {
@@ -164,48 +181,64 @@ export const useGetNftByHash = () => {
   const dispatch = useAppDispatch();
   const oAuthCtx = useContext(OAuthContext);
   const [loading, setLoading] = useState(false);
-  const [items, setItems] = useState<Record<string, any>>({});
+  const [items, setItems] = useState<Record<string, any>>({
+    image: '',
+    brand: '',
+    name: '',
+    description: '',
+    price: '',
+    usdPrice: '',
+    tokenAddress: '',
+    walletAddress: '',
+    is_listed: false,
+    attributes: [],
+    royaltiesPercentage: '',
+  });
 
   const getData = (token: string) => async (hash: string) => {
-    setLoading(true);
-    const response = await api.getNftListByTokenAddress(token, hash);
-    if (response?.data) {
-      dispatch({
-        type: 'SET_CURRENT_COLLECTION_TOKEN_DATA',
-        payload: response?.data,
-      });
-    }
-    const map =
-      response && Object.keys(response).length
-        ? [response].map((item: any) => {
-            const manifest = item?.splNftInfo?.data?.manifest;
-            return {
-              image: manifest?.image,
-              brand: manifest?.collection?.name,
-              name: manifest?.name,
-              description: manifest?.description,
-              price: item?.nftListings?.length
-                ? item?.nftListings[0].solPrice
-                : '',
-              usdPrice: item?.nftListings?.length
-                ? item?.nftListings[0].usdPrice
-                : '',
-              tokenAddress: item?.tokenAddress,
-              walletAddress: item?.splNftInfo?.walletAddress,
-              is_listed: item?.nftListings?.length,
-              attributes:
-                manifest?.attributes.map((item: any) => ({
-                  traitType: item.trait_type,
-                  value: item.value,
-                })) || [],
-              royaltiesPercentage:
-                manifest?.properties?.seller_fee_basis_points / 100,
-            };
-          })[0]
-        : {};
+    try {
+      setLoading(true);
+      const response = await api.getNftListByTokenAddress(token, hash);
+      if (response?.data) {
+        dispatch({
+          type: 'SET_CURRENT_COLLECTION_TOKEN_DATA',
+          payload: response?.data,
+        });
+      }
+      const transformed =
+        response && Object.keys(response).length
+          ? [response].map((item: any) => {
+              const manifest = item?.splNftInfo?.data?.manifest;
+              return {
+                image: manifest?.image,
+                brand: manifest?.collection?.name,
+                name: manifest?.name,
+                description: manifest?.description,
+                price: item?.nftListings?.length
+                  ? item?.nftListings[0].solPrice
+                  : '',
+                usdPrice: item?.nftListings?.length
+                  ? item?.nftListings[0].usdPrice
+                  : '',
+                tokenAddress: item?.tokenAddress,
+                walletAddress: item?.splNftInfo?.walletAddress,
+                is_listed: item?.nftListings?.length,
+                attributes:
+                  manifest?.attributes.map((item: any) => ({
+                    traitType: item.trait_type,
+                    value: item.value,
+                  })) || [],
+                royaltiesPercentage:
+                  manifest?.properties?.seller_fee_basis_points / 100,
+              };
+            })[0]
+          : {};
 
-    setItems(map);
-    setLoading(false);
+      setItems(transformed);
+      setLoading(false);
+    } catch {
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
