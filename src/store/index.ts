3ebd1a3c673ed76e94e6e5b1401a8a8a19150df8
irene <keiko15678@gmail.com>
2022-06-16 01:00:00 +0800

    refactor

--- src/store/index.ts
+++ src/store/index.ts
@@ -18,7 +18,7 @@ export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
 
-export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export default store;
