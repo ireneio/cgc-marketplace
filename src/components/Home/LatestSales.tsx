844dcd707b1e956ca1122a5ceabc1204a653842f
irene <keiko15678@gmail.com>
2022-06-17 14:49:10 +0800

    fix latest sales

--- src/components/Home/LatestSales.tsx
+++ src/components/Home/LatestSales.tsx
@@ -2,7 +2,6 @@ import { useEffect, useMemo, useState } from 'react';
 import SalesCard from './SalesCard';
 import SectionTitle from '../Shared/SectionTitle';
 import { useGetNftTransactionsV2 } from '@/hooks/services_nft';
-import dayjs from 'dayjs';
 import { useInView } from 'react-intersection-observer';
 import SalesCardLoading from './SalesCardLoading';
 import EmptyListTextDisplay from '../Shared/EmptyListTextDisplay';
@@ -10,14 +9,13 @@ import EmptyListTextDisplay from '../Shared/EmptyListTextDisplay';
 const LIMIT = 10;
 
 const LatestSales = () => {
-  const [items] = useState([1, 2, 3, 4, 5]);
   const { data, loading } = useGetNftTransactionsV2();
   const [page, setPage] = useState(0);
   const { inView, ref } = useInView();
 
   const _data = useMemo(() => {
     return data
-      .filter((nft: any) => nft?.nftEventId === 'buyNow')
+      .filter((nft: any) => nft?.nftEventType === 'buyNow')
       .slice(0, page + LIMIT);
   }, [data, inView, page]);
 
@@ -40,28 +38,28 @@ const LatestSales = () => {
         )}
         {!loading
           ? _data.map((tx, index) => {
-              return (
-                <div key={index} className="mr-[28px]">
-                  <SalesCard
-                    img={'/img/solchicks-641.png'}
-                    title={'SolChicks #641'}
-                    brand={'SolChicks'}
-                    signature={tx?.signature || ''}
-                    time={tx?.createdAt || ''}
-                    from={tx?.senderAddress || ''}
-                    to={tx?.recipientAddress || ''}
-                    amount={String(tx?.amountUsd) || ''}
-                  />
-                </div>
-              );
-            })
+            return (
+              <div key={index} className="mr-[28px]">
+                <SalesCard
+                  img={'/img/solchicks-641.png'}
+                  title={'SolChicks #641'}
+                  brand={'SolChicks'}
+                  signature={tx?.signature || ''}
+                  time={tx?.createdAt || ''}
+                  from={tx?.senderAddress || ''}
+                  to={tx?.recipientAddress || ''}
+                  amount={String(tx?.amountUsd) || ''}
+                />
+              </div>
+            );
+          })
           : [1, 2, 3, 4, 5].map((val, idx) => {
-              return (
-                <div key={idx} className="mr-[28px]">
-                  <SalesCardLoading />
-                </div>
-              );
-            })}
+            return (
+              <div key={idx} className="mr-[28px]">
+                <SalesCardLoading />
+              </div>
+            );
+          })}
         <div ref={ref}></div>
       </div>
     </div>
